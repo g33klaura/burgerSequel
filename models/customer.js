2f0498b373b6e6addf381acf2579abe645ae3c4a
Laura Boetcher <g33klaura@gmail.com>
2017-11-11 20:35:24 -0500

    Add customer model

---
+++ models/customer.js
@@ -0,0 +1,15 @@
+// Sequelize Customer model
+module.exports = function(sequelize, DataTypes) {
+	let Customer = sequelize.define('Customer', {
+		name: DataTypes.STRING
+	});
+
+/*
+	Customer.associate = function(models) {
+		Customer.hasMany(models.Burger, {
+			onDelete: 'cascade'
+		});
+	};
+*/
+	return Customer;
+};
