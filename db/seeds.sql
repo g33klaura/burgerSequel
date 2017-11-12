24dc8ca2ef71ed76dc6fbbd872d77fc20b406ec2
Laura Boetcher <g33klaura@gmail.com>
2017-11-11 21:55:00 -0500

    Update table names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Turkey patty, Swiss, grilled pineapple, spam, LTO, teriyaki sauce burger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon, American cheese, donut burger');
-INSERT INTO burgers (burger_name) VALUES ('Spicy black bean patty, avocado, LT, Aardvark hot sauce burger');
+INSERT INTO Burgers (burger_name) VALUES ('Turkey patty, Swiss, grilled pineapple, spam, LTO, teriyaki sauce burger');
+INSERT INTO Burgers (burger_name) VALUES ('Bacon, American cheese, donut burger');
+INSERT INTO Burgers (burger_name) VALUES ('Spicy black bean patty, avocado, LT, Aardvark hot sauce burger');
+
+-- Does this only get added by form tho... so there's an association to the burger...
+-- INSERT INTO Customers (name) VALUES ('Laura');
