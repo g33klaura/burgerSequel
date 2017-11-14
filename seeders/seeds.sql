cf0632d5b425bd7fb1148c3e53990217ea893e5f
Laura Boetcher <g33klaura@gmail.com>
2017-11-13 20:13:27 -0500

    Removed "seeds.sql" out of seeders directory

--- seeders/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Burgers (burger_name, CustomerId) VALUES ('Turkey patty, Swiss, grilled pineapple, spam, LTO, teriyaki sauce burger', 1);
-INSERT INTO Burgers (burger_name, CustomerId) VALUES ('Bacon, American cheese, donut burger', 1);
-INSERT INTO Burgers (burger_name, CustomerId) VALUES ('Spicy black bean patty, avocado, LT, Aardvark hot sauce burger', 1);
-
--- Does this only get added by form tho... so there's an association to the burger...
-INSERT INTO Customers (name) VALUES ('Laura Awesomesauce');
-
--- ALSO, does this get saved in the seeders directory???
